404c92479693134532f217cc11da6680fa1e378a
yeqiongzhou <yeqiongzhou@qiniu.com>
2021-03-10 10:07:12 +0800

    add logger

--- service/service.go
+++ service/service.go
@@ -1,11 +1,28 @@
 package service
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+
+	"paste.org.cn/paste/proto"
+	"paste.org.cn/paste/util"
+)
 
 type Paste struct{}
 
 func (p *Paste) PostPaste(c *gin.Context) {
+	var (
+		_, log = util.EnsureWithLogger(c)
+		req    proto.PostPasteReq
+	)
 
+	err := c.BindJSON(&req)
+	if err != nil {
+		log.Errorf("BindJSON failed: %+v", err)
+		c.JSON(http.StatusBadRequest, err.Error())
+		return
+	}
 }
 
 func (p *Paste) PostPasteOnce(c *gin.Context) {
---
+++ util/logger.go
@@ -0,0 +1,53 @@
+package util
+
+import (
+	"context"
+	"fmt"
+	"path"
+	"runtime"
+
+	"github.com/sirupsen/logrus"
+)
+
+func init() {
+	logrus.SetReportCaller(true)
+	logrus.SetFormatter(&logrus.TextFormatter{
+		FullTimestamp: true,
+		CallerPrettyfier: func(f *runtime.Frame) (string, string) {
+			filename := path.Base(f.File)
+			return fmt.Sprintf("%s()", f.Function), fmt.Sprintf("%s:%d", filename, f.Line)
+		},
+	})
+}
+
+type (
+	loggerKey struct{}
+)
+
+func GetLogger(ctx context.Context) *logrus.Entry {
+	logger := ctx.Value(loggerKey{})
+
+	if logger == nil {
+		return logrus.NewEntry(logrus.StandardLogger())
+	}
+
+	return logger.(*logrus.Entry)
+}
+
+func withLogger(ctx context.Context, logger *logrus.Entry) context.Context {
+	return context.WithValue(ctx, loggerKey{}, logger)
+}
+
+func EnsureWithLogger(ctx context.Context) (context.Context, *logrus.Entry) {
+	logger := ctx.Value(loggerKey{})
+
+	if logger == nil {
+		var rid string
+		ctx, rid = EnsureWithReqId(ctx)
+		logger = logrus.NewEntry(logrus.StandardLogger()).
+			WithField(REQID, rid)
+		ctx = withLogger(ctx, logger.(*logrus.Entry))
+	}
+
+	return ctx, logger.(*logrus.Entry)
+}
